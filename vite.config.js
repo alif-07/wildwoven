67217446d934061ddcaab71f84e014973114d310
Mohammad Alif <hashtagalif@gmail.com>
2025-03-30 18:30:13 -0700

    Initial commit for Wildwoven project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/landingPage/', // আপনার ডিপ্লয়মেন্ট URL-এর সাবডিরেক্টরি পাথ
+  base: '/wildwoven/', // আপনার ডিপ্লয়মেন্ট URL-এর সাবডিরেক্টরি পাথ
   build: {
     outDir: 'dist', // তৈরি হওয়া ফাইলগুলি কোন ডিরেক্টরিতে রাখা হবে
   },
